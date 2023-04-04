eca82cd778998c0fc35eae32d07daea9cde24a0d
Rolan <107186461+Kamikaze363@users.noreply.github.com>
2023-04-04 13:32:58 +0200

    Game.java update, Food.java made

--- src/main/java/com/example/demo/Game.java
+++ src/main/java/com/example/demo/Game.java
@@ -7,26 +7,38 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Game extends GameApplication {
     private Player player1;
     private Player player2;
+    private Food food;
 
     @Override
     protected void initSettings(GameSettings gameSettings) {
         gameSettings.setWidth(1000);
         gameSettings.setHeight(800);
+
         gameSettings.setTitle("Slither.io 1v1");
-        gameSettings.setVersion("0.2");
-        gameSettings.setProfilingEnabled(true);
+        gameSettings.setVersion("0.3");
+//        gameSettings.setProfilingEnabled(true);
     }
 
     @Override
     protected void initGame() {
         player1 = new Player(333, 400, Color.RED);
         player2 = new Player(666, 400, Color.BLUE);
+
         FXGL.getGameWorld().addEntity(player1.getEntity());
         FXGL.getGameWorld().addEntity(player2.getEntity());
+
+        FXGL.getGameTimer().runAtInterval(() -> {
+            int randomPosX = ThreadLocalRandom.current().nextInt(80, FXGL.getGameScene().getAppWidth() - 80);
+            int randomPosY = ThreadLocalRandom.current().nextInt(80, FXGL.getGameScene().getAppWidth() - 80);
+            food = new Food(randomPosX, randomPosY);
+        }, Duration.seconds(1));
 
         player1.move(KeyCode.A, KeyCode.D, KeyCode.W, KeyCode.S);
         player2.move(KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP, KeyCode.DOWN);
@@ -34,10 +46,10 @@ public class Game extends GameApplication {
 
     @Override
     protected void initPhysics() {
-        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.Player, EntityTypes.Player) {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.Player, EntityTypes.Food) {
             @Override
-            protected void onCollision(Entity player1, Entity player2) {
-
+            protected void onCollision(Entity player1, Entity food) {
+                food.removeFromWorld();
             }
         });
     }
