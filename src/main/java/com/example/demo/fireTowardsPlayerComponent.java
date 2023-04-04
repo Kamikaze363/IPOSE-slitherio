8cf1703159efe216380e758703376ad0279b5b4f
Albadri17 <117075465+Albadri17@users.noreply.github.com>
2023-04-04 11:55:37 +0200

    hoi

---
+++ src/main/java/com/example/demo/fireTowardsPlayerComponent.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.component.Component;
+import javafx.geometry.Point2D;
+
+
+public class fireTowardsPlayerComponent extends Component {
+    private final Entity player;
+
+    fireTowardsPlayerComponent (Entity player) {
+        this.player = player;
+    }
+    @Override
+    public void onUpdate(double tpf) {
+        Point2D playerPosition = player.getPosition();
+        Point2D  enenemyPosition = entity.getPosition();
+
+        Point2D direction = playerPosition.subtract(enenemyPosition).normalize();
+        entity.translate(direction.multiply(tpf * 100));
+    }
+}
