0a8bc2616aae66fa20849000ee8652a87656222f
Rolan <107186461+Kamikaze363@users.noreply.github.com>
2023-04-04 11:07:24 +0200

    game class

--- src/main/java/com/example/demo/Game.java
+++ src/main/java/com/example/demo/Game.java
@@ -2,14 +2,22 @@ package com.example.demo;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
 
 public class Game extends GameApplication {
+    private Entity player;
     @Override
     protected void initSettings(GameSettings gameSettings) {
         gameSettings.setWidth(800);
         gameSettings.setHeight(800);
         gameSettings.setTitle("Slither.io 1v1");
+        gameSettings.setVersion("0.1");
 
+    }
+
+    @Override
+    protected void initGame() {
+        super.initGame();
     }
 
     public static void main(String[] args) {
